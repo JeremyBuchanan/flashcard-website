6985c6f5548a8a6331969eecd2df70ce9ba72a9d
JeremyBuchanan <jer.buc@gmail.com>
2021-11-14 16:35:52 -0600

    added login functionality

--- flashcard/__init__.py
+++ flashcard/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECRET_KEY'] = '385c9dfa12c167b6c4bcb7be8004bba0'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flashcard import routes
